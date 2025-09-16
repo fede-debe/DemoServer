abdde26d3202d8783a09e382dcb582b55da0db8c
Federico De Benedictis <federico.debenedictis33@gmail.com>
2025-09-16 20:07:30 +0200

    new dependencies and version updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 val ktorVersion: String by project
+val kotlinVersion: String by project
 val logbackVersion: String by project
+val koinVersion: String by project
 
 plugins {
     application
     kotlin("jvm") version "2.1.21"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.1.21"
 }
 
 group = "com.example"
@@ -20,9 +23,21 @@ dependencies {
     testImplementation(kotlin("test"))
 
     implementation("io.ktor:ktor-server-core:${ktorVersion}")
-//    implementation("io.ktor:ktor-serialization-kotlinx-json:${ktorVersion}")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:${ktorVersion}")
     implementation("io.ktor:ktor-server-netty:${ktorVersion}")
     implementation("ch.qos.logback:logback-classic:${logbackVersion}")
+
+    implementation("io.ktor:ktor-server-call-logging:${ktorVersion}")
+    implementation("io.ktor:ktor-server-status-pages:${ktorVersion}")
+    implementation("io.ktor:ktor-server-content-negotiation:${ktorVersion}")
+    implementation("io.ktor:ktor-server-default-headers:${ktorVersion}")
+
+    testImplementation("io.ktor:ktor-server-test-host:${ktorVersion}")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:${kotlinVersion}")
+
+    implementation("io.insert-koin:koin-ktor:${koinVersion}")
+    implementation("io.insert-koin:koin-core:${koinVersion}")
+    implementation("io.insert-koin:koin-logger-slf4j:${koinVersion}")
 }
 
 tasks.test {
