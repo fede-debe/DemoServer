901f31aea350e5953cb884ba920750003c2f9d13
Federico De Benedictis <federico.debenedictis33@gmail.com>
2025-09-20 16:09:35 +0200

    new endpoints added to the main Routing to be used by the client

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,11 +1,17 @@
 package com.example.plugins
 
+import com.example.routes.getAllHeroes
 import com.example.routes.root
+import com.example.routes.searchHeroes
 import io.ktor.server.application.*
+import io.ktor.server.http.content.staticResources
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         root()
+        getAllHeroes()
+        searchHeroes()
+        staticResources(remotePath = "/images", basePackage = "images")
     }
 }
